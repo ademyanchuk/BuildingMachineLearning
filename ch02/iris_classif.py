e25f21a3e3c931b0cab80b16faf97decca4d8938
ademyanchuk <alexeydemyanchuk@gmail.com>
2017-04-30 22:05:44 +0700

    first classif model added

--- ch02/iris_classif.py
+++ ch02/iris_classif.py
@@ -1,6 +1,13 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+def is_virginica_test(fi, t, reverse, example):
+    "Apply threshold model to a new example"
+    test = example[fi] > t
+    if reverse:
+        test = not test
+    return test
+
 # We load the data with load_iris from sklearn
 
 from sklearn.datasets import load_iris
@@ -39,3 +46,34 @@ print('Minimum of others: {0}.'.format(min_non_setosa))
 
 
 plt.show()
+
+# ~ is the boolean negation operator
+features = features[~is_setosa]
+labels = labels[~is_setosa]
+
+# Build a new target variable, is_virginica
+is_virginica = (labels == "virginica")
+
+# Initialize best_acc to impossibly low value
+best_acc = -1.0
+
+for fi in range(features.shape[1]):
+    # We are going to test all possible thresholds
+    thresh = features[:,fi]
+    for t in thresh:
+        # Get the vector for feature `fi`
+        feature_i = features[:, fi]
+        # apply threshold `t`
+        pred = (feature_i > t)
+        acc = (pred == is_virginica).mean()
+        rev_acc = (pred == ~is_virginica).mean()
+        if rev_acc > acc:
+            reverse = True
+            acc = rev_acc
+        else:
+            reverse = False
+        if acc > best_acc:
+            best_acc = acc
+            best_fi = fi
+            best_t = t
+            best_reverse = reverse
