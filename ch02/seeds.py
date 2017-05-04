e9e87c5d1454338a1b19c7b37d3237154184e7d4
ademyanchuk <alexeydemyanchuk@gmail.com>
2017-05-04 23:00:00 +0700

    kNeigh classif started

---
+++ ch02/seeds.py
@@ -0,0 +1,33 @@
+from load import load_dataset
+
+feature_names = [
+    'area',
+    'perimeter',
+    'compactness',
+    'length of kernel',
+    'width of kernel',
+    'asymmetry coefficien',
+    'length of kernel groove',]
+
+features, labels = load_dataset('seeds')
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=1)
+
+from sklearn.cross_validation import KFold
+
+kf = KFold(len(features), n_folds=5, shuffle=True)
+# `means` will be a list of mean accuracies (one entry per fold)
+means = []
+for training,testing in kf:
+    # We fit a model for this fold, then apply it to the
+    # testing data with `predict`:
+    classifier.fit(features[training], labels[training])
+    prediction = classifier.predict(features[testing])
+
+    # np.mean on an array of booleans returns fraction
+    # of correct decisions for this fold:
+    curmean = np.mean(prediction == labels[testing])
+    means.append(curmean)
+
+print("Mean accuracy: {:.1%}".format(np.mean(means)))
