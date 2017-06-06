7473e304ffaecbf2e65fa43702fe8fd965e593eb
ademyanchuk <alexeydemyanchuk@gmail.com>
2017-06-06 22:51:52 +0700

    echobot LongPooling + good html encoding

--- Telegram_Bot/echobot.py
+++ Telegram_Bot/echobot.py
@@ -1,7 +1,8 @@
 #! /usr/local/bin/python3
-
+import time
 import json
 import requests
+import urllib
 
 TOKEN = '395213543:AAHwS2RJlp87c03PalqHRuVRgAnCP7wP7fM'
 URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
@@ -16,10 +17,18 @@ def get_json_from_url(url):
     js = json.loads(content)
     return js
 
-def get_updates():
-    url = URL + "getUpdates"
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
     js = get_json_from_url(url)
     return js
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
 
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
@@ -29,8 +38,28 @@ def get_last_chat_id_and_text(updates):
     return (text, chat_id)
 
 def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+def main():
+    last_update_id = None
+    while True:
+        print("getting updates")
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
