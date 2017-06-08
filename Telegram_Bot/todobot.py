42a178f6e06f927bc2f172eec5d9dc05b1071303
ademyanchuk <alexeydemyanchuk@gmail.com>
2017-06-08 23:04:07 +0700

    todobot handle_updates added

--- Telegram_Bot/todobot.py
+++ Telegram_Bot/todobot.py
@@ -70,7 +70,7 @@ def main():
         updates = get_updates(last_update_id)
         if len(updates["result"]) > 0:
             last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
+            handle_updates(updates)
         time.sleep(0.5)
 
 if __name__ == '__main__':
