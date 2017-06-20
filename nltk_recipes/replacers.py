a1f93f357f3ca0de1b6221b0b436bc4c19490e7a
ademyanchuk <alexeydemyanchuk@gmail.com>
2017-06-20 22:27:47 +0700

    antonym replacer added

--- nltk_recipes/replacers.py
+++ nltk_recipes/replacers.py
@@ -70,3 +70,34 @@ class YamlWordReplacer(WordReplacer):
      def __init__(self, fname):
        word_map = yaml.load(open(fname))
        super(YamlWordReplacer, self).__init__(word_map)
+
+class AntonymReplacer(object):
+    def replace(self, word, pos=None):
+        antonyms = set()
+        for syn in wordnet.synsets(word, pos=pos):
+            for lemma in syn.lemmas():
+                for antonym in lemma.antonyms():
+                    antonyms.add(antonym.name())
+        if len(antonyms) == 1:
+            return antonyms.pop()
+        else:
+            return None
+
+    def replace_negations(self, sent):
+        i, l = 0, len(sent)
+        words = []
+        while i < l:
+            word = sent[i]
+            if word == 'not' and i+1 < l:
+                ant = self.replace(sent[i+1])
+                if ant:
+                    words.append(ant)
+                    i += 2
+                    continue
+            words.append(word)
+            i += 1
+        return words
+
+class AntonymWordReplacer(WordReplacer, AntonymReplacer):
+    pass
+    
