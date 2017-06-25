d69b6dca7eb8e3708832159e4e7924fe8bca66e5
ademyanchuk <alexeydemyanchuk@gmail.com>
2017-06-25 22:01:25 +0700

    practice tagging

--- nltk_recipes/tag_util.py
+++ nltk_recipes/tag_util.py
@@ -1,4 +1,5 @@
 from nltk.probability import FreqDist, ConditionalFreqDist
+from nltk.tag import brill, brill_trainer
 
 def backoff_tagger(train_sents, tagger_classes, backoff=None):
      for cls in tagger_classes:
@@ -10,4 +11,26 @@ def word_tag_model(words, tagged_words, limit=200):
     cfd = ConditionalFreqDist(tagged_words)
     most_freq = (word for word, count in fd.most_common(limit))
     return dict((word, cfd[word].max()) for word in most_freq)
-    
+
+def train_brill_tagger(initial_tagger, train_sents, **kwargs):
+     templates = [
+       brill.Template(brill.Pos([-1])),
+       brill.Template(brill.Pos([1])),
+       brill.Template(brill.Pos([-2])),
+       brill.Template(brill.Pos([2])),
+       brill.Template(brill.Pos([-2, -1])),
+       brill.Template(brill.Pos([1, 2])),
+       brill.Template(brill.Pos([-3, -2, -1])),
+       brill.Template(brill.Pos([1, 2, 3])),
+       brill.Template(brill.Pos([-1]), brill.Pos([1])),
+       brill.Template(brill.Word([-1])),
+       brill.Template(brill.Word([1])),brill.Template(brill.Word([-2])),
+       brill.Template(brill.Word([2])),
+       brill.Template(brill.Word([-2, -1])),
+       brill.Template(brill.Word([1, 2])),
+       brill.Template(brill.Word([-3, -2, -1])),
+       brill.Template(brill.Word([1, 2, 3])),
+       brill.Template(brill.Word([-1]), brill.Word([1])),
+      ]
+     trainer = brill_trainer.BrillTaggerTrainer(initial_tagger, templates, deterministic=True, trace=True)
+     return trainer.train(train_sents, max_rules=1000, min_score=3, **kwargs)
